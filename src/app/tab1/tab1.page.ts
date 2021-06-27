992d2ef48056270cc92ee5de1ca0f376b5271c72
sahinaykkt <sahinaykkt@gmail.com>
2021-06-27 11:53:50 +0300

    Ders detay sayfa yapısı oluşturuldu.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,30 +1,29 @@
 import { Component } from '@angular/core';
 import { MyCourses } from '../Models/MyCourses';
 import { MyCoursesService } from '../services/my-courses.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor(private myCoursesService: MyCoursesService) {}
+  constructor(
+    private myCoursesService: MyCoursesService,
+    private router: Router
+  ) {}
 
   myCourses: MyCourses[];
 
-  ngOnInit(){
-    this.myCoursesService.myCourses().subscribe(
-      data => {
-        this.myCourses = data;
-        console.log(data);
-      }
-    );
+  ngOnInit() {
+    this.myCoursesService.myCourses().subscribe((data) => {
+      this.myCourses = data;
+      console.log(data);
+    });
   }
 
-
-  openDetails(courseId){
-    console.log(courseId);
+  openDetails(courseId) {
+    this.router.navigateByUrl('details', { replaceUrl: true });
   }
-
 }
