ace01de16f28e6f50bc96576c7f9891c4c5e5e75
sahinaykkt <sahinaykkt@gmail.com>
2021-06-28 18:02:25 +0300

    Ufak arayüz geliştirmeleri yapıldı.

--- src/app/Global.ts
+++ src/app/Global.ts
@@ -1,4 +1,4 @@
-export class Global
-{
-   static readonly baseUrl: string = 'http://192.168.1.60/api/';
-};
+export class Global {
+  static readonly baseUrl: string =
+    'http://halidsenyigit-001-site1.etempurl.com/api/';
+}
